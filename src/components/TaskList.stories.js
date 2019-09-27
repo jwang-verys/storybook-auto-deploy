78739d64464a923efa365afe71ebbff3a03a7735
Jeremy Wang <jeremywang@Jeremys-MacBook-Pro.local>
2019-09-26 17:15:11 -0700

    changed task 6 to task pinned

--- src/components/TaskList.stories.js
+++ src/components/TaskList.stories.js
@@ -12,7 +12,7 @@ export const defaultTasks = [
   { ...task, id: '3', title: 'Task 3' },
   { ...task, id: '4', title: 'Task 4' },
   { ...task, id: '5', title: 'Task 5' },
-  { ...task, id: '6', title: 'Task 6' },
+  { ...task, id: '6', title: 'Task Pinned' },
 ];
 
 export const withPinnedTasks = [
